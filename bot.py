0776c2a456e1336cc70b8a9381216c2f1579443a
fshstk <fshstk@fshstk.com>
2021-04-28 18:26:02 +0200

    Only keep the last 10 lines bot reply
    Discord has a 2048 char limit on embed descriptions so we need to avoid
    triggering an exception.

--- bot.py
+++ bot.py
@@ -30,16 +30,21 @@ async def reset_database(context):
     await drop_episodes(progress_callback=report_progress)
 
 
-async def amend_embed(message, content_to_add):
+async def amend_embed(message, content_to_add, max_lines=10):
     """
     Amend the passed `message` by adding `content_to_add` as a new line to the
     message embed's description. If no embed exists a new one will be added.
     (`message` must have type `discord.Message`)
     """
     timestamp = datetime.now().strftime("[%H:%M:%S]")
+    new_content = f"`{timestamp} {content_to_add}`"
+    print(new_content)  # Log the message to console...
+
     old_content = message.embeds[0].description if message.embeds else ""
-    new_content = f"{old_content}\n`{timestamp} {content_to_add}`"
-    new_embed = discord.Embed(description=new_content)
+    # Truncate down to the last (max_lines-1) lines:
+    old_content = "\n".join(old_content.splitlines()[-(max_lines - 1) :])
+
+    new_embed = discord.Embed(description="\n".join([old_content, new_content]))
     await message.edit(embed=new_embed)
 
 
