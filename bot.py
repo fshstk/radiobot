429fa6a30f8317921f1e1c6ae1be2a4a4bc52ead
fshstk <fshstk@fshstk.com>
2021-04-28 15:44:58 +0200

    Edit first message instead of sending multiple messages

--- bot.py
+++ bot.py
@@ -11,13 +11,23 @@ bot = commands.Bot(command_prefix=COMMAND_PREFIX)
 
 @bot.command(name="refresh")
 async def refresh_database(context):
-    report_progress = partial(send_message, context)
+    """
+    Update the episode database by scraping the CBA newsfeed. This may take a
+    long time, especially when called for the first time on an empty database,
+    but progress is reported periodically via the `progress_callback` function.
+    """
+    reply = await context.send("Refreshing database...")
+    report_progress = partial(amend_message, reply)
     await add_untracked_episodes(progress_callback=report_progress)
     await add_missing_mp3_urls(progress_callback=report_progress)
 
 
-async def send_message(context, message):
-    await context.send(message)
+async def amend_message(message, string):
+    """
+    Amend the passed `message`, replacing its contents with `string`.
+    (`message` must have type `discord.Message`)
+    """
+    await message.edit(content=string)
 
 
 print("Running bot...")
