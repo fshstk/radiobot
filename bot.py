b89145b258c95f8ee812add91f5f90bb25aafe98
fshstk <fshstk@fshstk.com>
2021-04-28 12:47:17 +0200

    Implement bot command for database refresh
    - print() statements were replaced by an async callback function in
    database
    - now the bot can call the functions and supply a callback that prints
    the progress reports as a message in discord chat

--- bot.py
+++ bot.py
@@ -1,16 +1,23 @@
 import discord
 from discord.ext import commands, tasks
+from functools import partial
 
 from config import BOT_TOKEN, COMMAND_PREFIX
+from database import add_missing_mp3_urls, add_untracked_episodes
 
 client = discord.Client()
 bot = commands.Bot(command_prefix=COMMAND_PREFIX)
 
 
-@bot.command(name="radio")
-async def radio(ctx):
-    await ctx.send(f"hello {ctx.message.author.name}!")
-    return
+@bot.command(name="refresh")
+async def refresh_database(context):
+    report_progress = partial(send_message, context)
+    await add_untracked_episodes(progress_callback=report_progress)
+    await add_missing_mp3_urls(progress_callback=report_progress)
+
+
+async def send_message(context, message):
+    await context.send(message)
 
 
 print("Running bot...")
--- database.py
+++ database.py
@@ -8,14 +8,15 @@ from scraper import scrape_episodes, scrape_mp3_url
 mongoengine.connect(host=DATABASE_URL)
 
 
-def add_untracked_episodes():
+async def add_untracked_episodes(progress_callback=None):
     """
     Scrapes the RSS feed for all available episodes, then tries to add them to
     the database. Existing episodes will be rejected by the database backend as
     each episode must have a unique page url.
     """
     episodes = scrape_episodes()
-    print(f"Scraper found {len(episodes)} episodes in feed.")
+    if progress_callback is not None:
+        await progress_callback(f"Scraper found {len(episodes)} episodes in feed.")
 
     update_counter = 0
     duplicate_counter = 0
@@ -27,10 +28,13 @@ def add_untracked_episodes():
         except NotUniqueError:
             duplicate_counter += 1
 
-    print(f"Added {update_counter} entries. {duplicate_counter} were duplicates.")
+    if progress_callback is not None:
+        await progress_callback(
+            f"Added {update_counter} entries. {duplicate_counter} were duplicates."
+        )
 
 
-def add_missing_mp3_urls():
+async def add_missing_mp3_urls(progress_callback=None):
     """
     Checks which episodes do not have a url to the episode audio and tries to
     update them. This involves a separate page scrape for EACH episode we are
@@ -39,9 +43,12 @@ def add_missing_mp3_urls():
     episodes_to_process = Episode.missing_audio
 
     if len(episodes_to_process) == 0:
-        print("No episodes to process")
+        if progress_callback is not None:
+            await progress_callback("No episodes to process")
         return
     for number, ep in enumerate(episodes_to_process):
-        print(f"Processing episode {number+1} of {len(episodes_to_process)}...")
+        if progress_callback is not None:
+            await progress_callback(
+                f"Processing episode {number+1} of {len(episodes_to_process)}..."
+            )
         ep.update(mp3_url=scrape_mp3_url(ep))
-    print("Done")
